7e1ad17cb68c0483da302538928c4a0c7ef00ab1
Kayla Lebron <kayla.lebron12@gmail.com>
2023-05-08 21:43:36 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+
+let random = Number.isInteger(Math.random())+ 1
+
+let num3 = 34
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(1,2,3,4,5,6,7,8,20) 
